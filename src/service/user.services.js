59b0403377379fd96ac45eeb846f39501961f147
Israel-ops-cmd <israellipe2020@gmail.com>
2025-05-29 07:46:35 -0300

    feat: bcrypt and nodemon

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,7 +1,18 @@
 import userRepository from '../repositories/user.repositories.js'
+import bcrypt from 'bcrypt'
 
 async function createUserService(newUser) {
-    const user = await userRepository.createUserRepository(newUser)
+    const foundUserName = await userRepository.findUserByUsernameRepository(newUser.username)
+    const foundUser = await userRepository.findUserByEmailRepository(newUser.email)
+    if(foundUser) {
+        throw new Error("User already exists!")
+    }
+    else if(foundUserName) {
+        throw new Error("User already exists!")
+    }
+    const passHash = await bcrypt.hash(newUser.password, 10)
+    const user = await userRepository.createUserRepository({...newUser, password: passHash})
+    if(!user) throw new Error("Error creating User!")
     return user
 }
 
